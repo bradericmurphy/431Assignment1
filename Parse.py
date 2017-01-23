db7091c7901d0586eca089ea5f00415f553f5fcb
bradericmurphy <bradleyericmurphy@gmail.com>
2017-01-23 12:32:34 -0500

    Simplified email regex

--- Parse.py
+++ Parse.py
@@ -2,7 +2,7 @@ import sys, re, io
 inPt = sys.stdin
 
 
-pattern = r"(^[\w.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+pattern = r"(^[\w.+-]+@[\w-]+\.[\w-.]+$)"
 
 for pattern in inPt:
-    print ("yes!")
+    print (pattern.rstrip("\r\n"))
