e78ecd63a67d963c1e43ac42e1dbca2dfd85eae2
bradericmurphy <bradleyericmurphy@gmail.com>
2017-01-25 01:35:36 -0500

    added more checks

--- Parse.py
+++ Parse.py
@@ -22,19 +22,27 @@ for line in sys.stdin:
 #    print (pattern.rstrip("\r\n"))
 
 def checkIfNum(character):
-    numpattern = r"\d"
+    numpattern = "\d"
     character.match(numpattern)
     #Not Finished??
 
 def checkIfSpecial(character):
-    specialcharpattern = r"[[]<>()\.,;:@]"
-    #DOES NOT INCLUDE QUOTE MARKS
-
-def checkIfNonSpecial(character)
-    val = ord(character)
     if (val == 60 | val == 62 | val == 40 | val == 41 | val == 91 | val == 93 | val == 92 | val == 46 | val == 44 | val == 59 | val == 58 | val == 64 | val == 34):
         #return boolean
 
+def checkIfNonSpecial(character)
+    val = ord(character)
+    if ((val < 128) & (val == 60 | val == 62 | val == 40 | val == 41 | val == 91 | val == 93 | val == 92 | val == 46 | val == 44 | val == 59 | val == 58 | val == 64 | val == 34 | val == 32)):
+        #return boolean
+
 def sp(character):
-    val = org(character)
-    if (val = space)
+    val = ord(character)
+    if (val = 32 | val = 9):
+        #return boolean
+
+def checkIfA():
+    apattern = "[A-Za-z]"
+    character.match(apattern)
+    #Not Finished
+
+
