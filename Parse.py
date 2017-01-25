a47b06f0bd34cf29be3fa80bc08e239a5d234fd7
bradericmurphy <bradleyericmurphy@gmail.com>
2017-01-25 01:23:51 -0500

    retooled grammar, added ASCII value comparators

--- Parse.py
+++ Parse.py
@@ -1,8 +1,40 @@
-import sys, re, io
-inPt = sys.stdin
+import sys, re, io, string
 
 
-pattern = r"(^[\w.+-]+@[\w-]+\.[\w-.]+$)"
 
-for pattern in inPt:
-    print (pattern.rstrip("\r\n"))
+emailPattern = r"(^[\w.+-]+@[\w-]+\.[\w-.]+$)"
+
+mailFrom = "^MAIL[\s\t]+FROM:"
+
+for line in sys.stdin:
+
+
+
+    #print (line.rstrip("\r\n"))
+    #fromStr = line.split(' ', 1)[0]
+    #print(fromStr)
+
+
+
+#Then do the loop
+
+#for pattern in in:
+#    print (pattern.rstrip("\r\n"))
+
+def checkIfNum(character):
+    numpattern = r"\d"
+    character.match(numpattern)
+    #Not Finished??
+
+def checkIfSpecial(character):
+    specialcharpattern = r"[[]<>()\.,;:@]"
+    #DOES NOT INCLUDE QUOTE MARKS
+
+def checkIfNonSpecial(character)
+    val = ord(character)
+    if (val == 60 | val == 62 | val == 40 | val == 41 | val == 91 | val == 93 | val == 92 | val == 46 | val == 44 | val == 59 | val == 58 | val == 64 | val == 34):
+        #return boolean
+
+def sp(character):
+    val = org(character)
+    if (val = space)
