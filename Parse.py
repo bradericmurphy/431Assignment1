fe805bc39b887af2cc9975a4d079a38b45362169
bradericmurphy <bradleyericmurphy@gmail.com>
2017-01-26 01:33:17 -0500

    retooled grammar checks, progressed through local-part

--- Parse.py
+++ Parse.py
@@ -1,55 +1,39 @@
 import sys, re, io, string
-charCounter = 0
+
 
 
 mailFrom = "^MAIL[\s]+FROM:"
 mailFromPattern = re.compile(mailFrom)
 
-for line in sys.stdin:
-    print(line.rstrip("\n"))
-    strList = line.split(':', 1)
-    mailFromString = strList[0]+':'
-    if (not (bool(mailFromPattern.match(mailFromString)))):
-        print("ERROR -- mail-from-cmd")
+num = "\d"
+numPattern = re.compile(num)
 
-    restOfString = strList[1].lstrip()
-    if
+whitespace = "[\t\s\r\f\v]"
+whitespacePattern = re.compile(whitespace)
 
+special = "[][<>(),.;:@\\\\'\"']"
+specialPattern = re.compile(special)
 
-
-    #print (line.rstrip("\r\n"))
-    #fromStr = line.split(' ', 1)[0]
-    #print(fromStr)
-
-
-
-#Then do the loop
-
-#for pattern in in:
-#    print (pattern.rstrip("\r\n"))
-
+a = "[A-Za-z]"
+aPattern = re.compile(a)
 
 def d(character):
-    numpattern = "\d"
-    return bool(character.match(numpattern))
+    return bool(character.match(numPattern))
 
-
-def special(character):
-    val = ord(character)
-    return (val == 60 | val == 62 | val == 40 | val == 41 | val == 91 | val == 93 | val == 92 | val == 46 | val == 44 | val == 59 | val == 58 | val == 64 | val == 34)
-
+def special(thisstring):
+    return bool(specialPattern.search(thisstring))
+    #val = ord(character)
+    #return bool(val == 60 | val == 62 | val == 40 | val == 41 | val == 91 | val == 93 | val == 92 | val == 46 | val == 44 | val == 59 | val == 58 | val == 64 | val == 34)
 
 def c(character):
     val = ord(character)
-    return ((val < 128) & (val != 60 | val != 62 | val != 40 | val != 41 | val != 91 | val != 93 | val != 92 | val != 46 | val != 44 | val != 59 | val != 58 | val != 64 | val != 34 | val != 32))
+    return bool((val < 128) & (val != 60 | val != 62 | val != 40 | val != 41 | val != 91 | val != 93 | val != 92 | val != 46 | val != 44 | val != 59 | val != 58 | val != 64 | val != 34 | val != 32))
 
-def sp(character):
-    val = ord(character)
-    return (val == 32 | val == 9)
+def sp(thisstring):
+    return bool(whitespacePattern.search(thisstring))
 
 def a(character):
-    apattern = "[A-Za-z]"
-    return bool(character.match(apattern))
+    return bool(aPattern.match(character))
 
 def charCheck(character):
     return c(character)
@@ -65,3 +49,43 @@ def letDig(character):
  #   if (bool(a(character)))
 
 #def MailFromCmd():
+
+for line in sys.stdin:
+    print(line.rstrip("\n"))
+    strList = line.split(':', 1)
+    mailFromString = strList[0]+':'
+    if (not (bool(mailFromPattern.match(mailFromString)))):
+        print("ERROR -- mail-from-cmd")
+        continue
+
+
+    restOfString = strList[1].lstrip()
+    if ord(restOfString[0]) != 60:
+        print("ERROR -- path")
+        continue
+
+    strList = restOfString.split('@', 1)
+    strin = strList[0]
+    remainingStr = '@' + strList[1]
+    strin = strin.lstrip('<')
+    stopHereCheck = sp(strin)
+    if special(strin):
+        stopHereCheck = True
+
+    if stopHereCheck == True:
+        print("ERROR -- local-part")
+       #print(strin + str(len(strin)))#used for testing
+        continue
+
+
+
+
+
+
+
+
+    #print (line.rstrip("\r\n"))
+    #fromStr = line.split(' ', 1)[0]
+    #print(fromStr)
+
+
